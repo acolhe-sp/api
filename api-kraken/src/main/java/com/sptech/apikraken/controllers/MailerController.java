5a4451b48e62f6e4fe29fdc05ac41489f725d8b0
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2022-04-10 19:21:22 -0300

    send mails service

---
+++ api-kraken/src/main/java/com/sptech/apikraken/controllers/MailerController.java
@@ -0,0 +1,28 @@
+package com.sptech.apikraken.controllers;
+
+import com.sptech.apikraken.dto.MailerDTO;
+import com.sptech.apikraken.entity.Mailer;
+import com.sptech.apikraken.services.MailerService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/sendmail")
+public class MailerController {
+    @Autowired MailerService mailerService;
+
+    @PostMapping
+    public ResponseEntity<Mailer> sendigMail(@RequestBody @Valid MailerDTO emailDTO) {
+        Mailer email = new Mailer();
+        BeanUtils.copyProperties(emailDTO, email);
+        mailerService.sendMail(email);
+        return ResponseEntity.status(200).build();
+    }
+}
---
+++ api-kraken/src/main/java/com/sptech/apikraken/entity/Mailer.java
@@ -0,0 +1,49 @@
+package com.sptech.apikraken.entity;
+
+public class Mailer {
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    private String text;
+
+    public Mailer(String emailFrom, String emailTo, String subject, String text) {
+        this.emailFrom = emailFrom;
+        this.emailTo = emailTo;
+        this.subject = subject;
+        this.text = text;
+    }
+
+    public Mailer() {}
+
+    public String getEmailFrom() {
+        return emailFrom;
+    }
+
+    public void setEmailFrom(String emailFrom) {
+        this.emailFrom = emailFrom;
+    }
+
+    public String getEmailTo() {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
